49170a8ad455cfdbc9b547b5b4381e56190c3305
SomaiaElbaradey <somayaelbaradey@gmail.com>
2021-02-16 21:07:57 +0200

    go production.

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const userRouter = require('./routes/user');
 const countryRouter = require('./routes/country');
 
 app.get("/*", (req, res)=>{
-    res.sendFile(process.cwd() + "/dist/index.html");
+    res.sendFile(path.join(__dirname, "/dist/Covid19Front/"));
 })
 app.use(express.static('public'));
 app.use(express.json());
