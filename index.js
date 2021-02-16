a64a6fbe58066b854f1eca5179e67c87ab317800
SomaiaElbaradey <somayaelbaradey@gmail.com>
2021-02-16 22:21:38 +0200

    pro.

--- index.js
+++ index.js
@@ -7,17 +7,24 @@ const app = express();
 const error = require('./middlewares/error');
 const logs = require('./middlewares/logs');
 
-if(!config.get('jwtKey')){
+if (!config.get('jwtKey')) {
     console.log("FATAL ERROR: jwtKey is not defined.")
     process.exit(1);
-} 
+}
 //routes
 const userRouter = require('./routes/user');
 const countryRouter = require('./routes/country');
 
-app.get("/*", (req, res)=>{
-    res.sendFile(process.cwd() + "/dist/Covid19Front/index.html");
-})
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/Covid19Front'));
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/Covid19Front/index.html'));
+});
+
+// app.get("/*", (req, res)=>{
+//     res.sendFile(process.cwd() + "/dist/Covid19Front/index.html");
+// })
 app.use(express.static('public'));
 app.use(express.json());
 app.use(logs);
